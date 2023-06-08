e66f8e4d62a0e83d6d483be3f02933b26472ea8b
0xBipBip <oxbipbip@gmail.com>
2023-06-08 20:33:55 -0300

    27

--- 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
+++ 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
@@ -163,4 +163,109 @@ multi-line comment */
 // Now let's divide one decimal by another.
 // const quotient = 4.4 / 2.0;
 
-20 - 
+20 - Finding a Remainder in JavaScript
+
+// The remainder operator % gives the remainder of the division of two numbers.
+//     5 % 2 = 1
+//     5 / 2 = 2 remainder 1
+// USAGE: In mathematics, a number can be checked to be even or odd by checking the remainder of the division 
+// of the number by 2. Even numbers have a remainder of 0, while odd numbers a remainder of 1.
+//     17 % 2 = 1
+//     48 % 2 = 0
+// Note: The remainder operator is sometimes incorrectly referred to as the modulus operator. It is 
+// very similar to modulus, but does not work properly with negative numbers.
+
+21 - Compound Assignment With Augmented Addition
+
+// In programming, it is common to use assignments to modify the contents of a variable. Remember that 
+// everything to the right of the equals sign is evaluated first, so we can say:
+//     myVar = myVar + 5;
+// to add 5 to myVar. Since this is such a common pattern, there are operators which do both a 
+// mathematical operation and assignment in one step.
+
+// One such operator is the += operator.
+//     let myVar = 1;
+//     myVar += 5;
+//     console.log(myVar);
+// 6 would be displayed in the console.
+
+22 - Compound Assignment With Augmented Subtraction
+
+// Like the += operator, -= subtracts a number from a variable.
+//     myVar = myVar - 5;
+// will subtract 5 from myVar. This can be rewritten as:
+//     myVar -= 5;
+
+23 - Compound Assignment With Augmented Multiplication
+
+// The *= operator multiplies a variable by a number.
+//     myVar = myVar * 5;
+// will multiply myVar by 5. This can be rewritten as:
+//     myVar *= 5;
+
+24 - Compound Assignment With Augmented Division
+
+// The /= operator divides a variable by another number.
+//     myVar = myVar / 5;
+// Will divide myVar by 5. This can be rewritten as:
+//     myVar /= 5;
+
+25 - Escaping Literal Quotes in Strings
+
+// When you are defining a string you must start and end with a single or double quote. What happens 
+// when you need a literal quote: " or ' inside of your string?
+// In JavaScript, you can escape a quote from considering it as an end of string quote by placing a 
+// backslash (\) in front of the quote.
+//     const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+// This signals to JavaScript that the following quote is not the end of the string, but should 
+// instead appear inside the string. So if you were to print this to the console, you would get:
+//     Alan said, "Peter is learning JavaScript".
+// Solution:   const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+26 - Quoting Strings with Single Quotes
+
+// String values in JavaScript may be written with single or double quotes, as long as you start and 
+// end with the same type of quote. Unlike some other programming languages, single and double quotes 
+// work the same in JavaScript.
+//     const doubleQuoteStr = "This is a string"; 
+//     const singleQuoteStr = 'This is also a string';
+// The reason why you might want to use one type of quote over the other is if you want to use both 
+// in a string. This might happen if you want to save a conversation in a string and have the 
+// conversation in quotes. Another use for it would be saving an <a> tag with various attributes in 
+// quotes, all within a string.
+//     const conversation = 'Finn exclaims to Jake, "Algebraic!"';
+// However, this becomes a problem if you need to use the outermost quotes within it. Remember, a 
+// string has the same kind of quote at the beginning and end. But if you have that same quote 
+// somewhere in the middle, the string will stop early and throw an error.
+//     const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+//     const badStr = 'Finn responds, "Let's go!"';
+// Here badStr will throw an error.
+// In the goodStr above, you can use both quotes safely by using the backslash \ as an escape character.
+// Note: The backslash \ should not be confused with the forward slash /. They do not do the same thing.
+// PROBLEM: const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+// SOLUTION: const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+27 -  Escape Sequences in Strings
+
+// Quotes are not the only characters that can be escaped inside a string. Escape sequences allow you 
+// to use characters you may not otherwise be able to use in a string.
+
+// Code	Output
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \t	tab
+// \r	carriage return
+// \b	backspace
+// \f	form feed
+// Note that the backslash itself must be escaped in order to display as a backslash.
+
+// PROBLEM: Assign the following three lines of text into the single variable myStr using escape 
+// sequences.
+// FirstLine
+//     \SecondLine
+// ThirdLine
+// SOLUTION: const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+28 - 
