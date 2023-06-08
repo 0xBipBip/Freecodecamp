c282cc2cb3ef64b599ba15c304c021c494910bda
0xBipBip <oxbipbip@gmail.com>
2023-06-08 11:45:29 -0300

    JS basics 01-06

---
+++ 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
@@ -0,0 +1,63 @@
+01 - Comment Your JavaScript Code
+
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+02 - Declare JavaScript Variables
+
+// JavaScript provides eight different data types which are undefined, null, boolean, string, 
+// symbol, bigint, number, and object.
+
+// Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or 
+// start with a number.
+
+// You can declare variables using the var, let, or const keywords. 
+
+// var firstName = 'John';
+// var age = 30;
+// var isEmployed = true;
+
+// let lastName = 'Doe';
+// let height = 180;
+// let isStudent = false;
+
+// const PI = 3.14;
+// const MAX_VALUE = 100;
+
+// Note that var has function scope, while let and const have block scope. Additionally, var v
+// ariables can be reassigned and hoisted (moved to the top of their scope), while let and const 
+// variables are block-scoped and cannot be redeclared in the same scope.
+
+03 - Storing Values with the Assignment Operator
+
+// you can store a value in a variable with the assignment operator (=).
+// myVariable = 5;
+// If there are any calculations to the right of the = operator, those are performed before the 
+// value is assigned to the variable on the left of the operator.
+
+04 - Assigning the Value of One Variable to Another
+
+// After a value is assigned to a variable using the assignment operator, you can assign the value 
+// of that variable to another variable using the assignment operator.
+
+// var myVar;
+// myVar = 5;
+// var myNum;
+// myNum = myVar;
+
+05 - Initializing Variables with the Assignment Operator
+
+// It is common to initialize a variable to an initial value in the same line as it is declared.
+// var myVar = 0;
+
+06 - Declare String Variables
+
+// You can declare a string variable like this:
+// var myName = "your name";
+
+// "your name" is called a string literal. A string literal, or string, is a series of zero or more 
+// characters enclosed in single or double quotes.
+
+07 - Understanding Uninitialized Variables
