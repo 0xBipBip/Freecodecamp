ca64a44087498b31ab745d348a520b74de6d55f0
0xBipBip <oxbipbip@gmail.com>
2023-07-05 15:08:27 -0300

    profile lookup

--- 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
+++ 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
@@ -2608,20 +2608,77 @@ again (in this example, when n <= 0), otherwise they can never finish executing.
 
 PROBLEM: Write a recursive function, sum(arr, n), that returns the sum of 
 the first n elements of an array arr.
-*/
+
+SOLUTION:
 function sum(arr, n) {
   if (n == 0) {
     return 0;
   } else {
-    console.log(arr[n-1]);
-    return sum(arr, n-1) + arr[n - 1];
+    return arr[n - 1] + sum(arr, n-1);
   }
 }
 
 sum([1, 3, 5, 7, 8, 11, 14], 7); // 9
+*/
 
+/* 104 - Profile Lookup
+We have an array of objects representing different people in our contacts lists.
+A lookUpProfile function that takes name and a property (prop) as arguments has been 
+pre-written for you.
+The function should:
+-Check if name is an actual contact's firstName and the given property 
+(prop) is a property of that contact.
+OK - If both are true, then return the "value" of that property.
+- If name does not correspond to any contacts then return the string No such contact.
+- If prop does not correspond to any valid properties of a contact found to match 
+name then return the string No such property.
+*/
 
-/* 104 105 106 107 108 109 110 */
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+SOLUTION:
+function lookUpProfile(name, prop) {
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+lookUpProfile("Akira", "lastNamess");
+
+//- If name does not correspond to any contacts then return the string No such contact.
+/* 105 106 107 108 109 110 */
 
 
 
