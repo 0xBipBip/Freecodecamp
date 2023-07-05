aae30190104e24fb5a28266e4374bd51fb5edd77
0xBipBip <oxbipbip@gmail.com>
2023-07-05 11:32:14 -0300

    recursive sum

--- 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
+++ 02 - Java Script Algorithms and Data Structures/01 - Basic Javascript/script.js
@@ -2578,7 +2578,9 @@ while (i < 5);
 */
 
 /* 103 - Replace Loops using Recursion
-Recursion is the concept that a function can be expressed in terms of itself. To help understand this, start by thinking about the following task: multiply the first n elements of an array to create the product of those elements. Using a for loop, you could do this:
+Recursion is the concept that a function can be expressed in terms of itself. To help understand this, 
+start by thinking about the following task: multiply the first n elements of an array to create 
+the product of those elements. Using a for loop, you could do this:
 
   function multiply(arr, n) {
     let product = 1;
@@ -2587,7 +2589,8 @@ Recursion is the concept that a function can be expressed in terms of itself. To
     }
     return product;
   }
-However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That means you can rewrite multiply in terms of itself and never need to use a loop.
+However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That means you can 
+rewrite multiply in terms of itself and never need to use a loop.
 
   function multiply(arr, n) {
     if (n <= 0) {
@@ -2596,16 +2599,26 @@ However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That
       return multiply(arr, n - 1) * arr[n - 1];
     }
   }
-The recursive version of multiply breaks down like this. In the base case, where n <= 0, it returns 1. For larger values of n, it calls itself, but with n - 1. That function call is evaluated in the same way, calling multiply again until n <= 0. At this point, all the functions can return and the original multiply returns the answer.
+The recursive version of multiply breaks down like this. In the base case, where n <= 0, it 
+returns 1. For larger values of n, it calls itself, but with n - 1. That function call is 
+evaluated in the same way, calling multiply again until n <= 0. At this point, all the 
+functions can return and the original multiply returns the answer.
+Note: Recursive functions must have a base case when they return without calling the function 
+again (in this example, when n <= 0), otherwise they can never finish executing.
 
-Note: Recursive functions must have a base case when they return without calling the function again (in this example, when n <= 0), otherwise they can never finish executing.
-
-Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+PROBLEM: Write a recursive function, sum(arr, n), that returns the sum of 
+the first n elements of an array arr.
+*/
 function sum(arr, n) {
-  // Only change code below this line
-
-  // Only change code above this line
+  if (n == 0) {
+    return 0;
+  } else {
+    console.log(arr[n-1]);
+    return sum(arr, n-1) + arr[n - 1];
+  }
 }
+
+sum([1, 3, 5, 7, 8, 11, 14], 7); // 9
 
 
 /* 104 105 106 107 108 109 110 */
